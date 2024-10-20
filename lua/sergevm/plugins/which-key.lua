b57d95e8f6502c63e8c0dde9840890c9dd1c3835
Serge Van Meerbeeck <serge.vanmeerbeeck@gmail.com>
2024-10-20 22:51:09 +0200

    terminal

--- lua/sergevm/plugins/which-key.lua
+++ lua/sergevm/plugins/which-key.lua
@@ -1,18 +1,25 @@
-return   { -- Useful plugin to show you pending keybinds.
-    'folke/which-key.nvim',
-    event = 'VimEnter', -- Sets the loading event to 'VimEnter'
-    config = function() -- This is the function that runs, AFTER loading
-      require('which-key').setup()
+return { -- Useful plugin to show you pending keybinds.
+  'folke/which-key.nvim',
+  event = 'VimEnter', -- Sets the loading event to 'VimEnter'
+  config = function() -- This is the function that runs, AFTER loading
+    local wk = require 'which-key'
 
-      -- Document existing key chains
-      require('which-key').add {
-        { '<leader>c', group = '[C]ode' },
-        { '<leader>d', group = '[D]ocument' },
-        { '<leader>r', group = '[R]ename' },
-        { '<leader>s', group = '[S]earch' },
-        { '<leader>w', group = '[W]orkspace' },
-        { '<leader>t', group = '[T]oggle' },
-        { '<leader>h', group = 'Git [H]unk', mode = { 'n', 'v' } },
-      }
-    end,
-  }
+    wk.setup()
+
+    wk.add {
+      { '<leader>c', group = '[C]ode' },
+      { '<leader>d', group = '[D]ocument' },
+      { '<leader>r', group = '[R]ename' },
+      { '<leader>s', group = '[S]earch' },
+      { '<leader>w', group = '[W]orkspace' },
+      { '<leader>t', group = '[T]oggle' },
+      { '<leader>h', group = 'Git [H]unk', mode = { 'n', 'v' } },
+    }
+
+    wk.add {
+      { '<leader>tt', '<cmd>ToggleTerm direction=float<cr>', desc = 'Terminal Float' },
+      { '<leader>tv', '<cmd>ToggleTerm size=80 direction=vertical<cr>', desc = 'Terminal Vertical' },
+      { '<leader>th', '<cmd>ToggleTerm size=80 direction=horizontal size=20<cr>', desc = 'Terminal Horizontal' },
+    }
+  end,
+}
