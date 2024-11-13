c985cf78cfd157df13fc1ae60a5754e25cbb1015
Serge Van Meerbeeck <serge.vanmeerbeeck@gmail.com>
2024-11-13 19:22:39 +0100

    disable toggle term. add key mappings.

--- lua/sergevm/core/keymaps.lua
+++ lua/sergevm/core/keymaps.lua
@@ -14,6 +14,7 @@ vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left wind
 vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+vim.keymap.set('i', 'jk', '<ESC>', { desc = 'Escape to normal mode' })
 
 keymap.set('n', '<leader>+', '<C-a>', { desc = 'Increment number' })
 keymap.set('n', '<leader>-', '<C-x>', { desc = 'Increment number' })
@@ -24,11 +25,19 @@ keymap.set('n', '<leader>sh', '<C-w>s', { desc = 'Split window horizontally' })
 keymap.set('n', '<leader>se', '<C-w>=', { desc = 'Make splits equal size' })
 keymap.set('n', '<leader>sx', '<cmd>close<CR>', { desc = 'Close current split' })
 
-keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab" }) -- open new tab
-keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" }) -- close current tab
-keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" }) --  go to next tab
-keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" }) --  go to previous tab
-keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" }) --  move current buffer to new tab
+keymap.set('n', '<leader>to', '<cmd>tabnew<CR>', { desc = 'Open new tab' }) -- open new tab
+keymap.set('n', '<leader>tx', '<cmd>tabclose<CR>', { desc = 'Close current tab' }) -- close current tab
+keymap.set('n', '<leader>tn', '<cmd>tabn<CR>', { desc = 'Go to next tab' }) --  go to next tab
+keymap.set('n', '<leader>tp', '<cmd>tabp<CR>', { desc = 'Go to previous tab' }) --  go to previous tab
+keymap.set('n', '<leader>tf', 'u-[[ <cmd>tabnew %<C ]]R>', { desc = 'Open current buffer in new tab' }) --  move current buffer to new tab
+
+-- WORKS, but is awefully slow ...
+-- keymap.set('i', 'j', function()
+--   return vim.fn.pumvisible() and '<C-n>' or 'j'
+-- end, { silent = true, expr = true })
+-- keymap.set('i', 'k', function()
+--   return vim.fn.pumvisible() and '<C-n>' or 'k'
+-- end, { silent = true, expr = true })
 
 vim.api.nvim_create_autocmd('TextYankPost', {
   desc = 'Highlight when yanking (copying) text',
