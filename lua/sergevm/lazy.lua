f1f2559b362d00725979bd9172278f4b06dd2c5c
Serge Van Meerbeeck <serge.vanmeerbeeck@gmail.com>
2025-05-04 18:02:29 +0200

    Switching theme to Catppuchin

--- lua/sergevm/lazy.lua
+++ lua/sergevm/lazy.lua
@@ -21,8 +21,9 @@ require('lazy').setup {
     { import = 'sergevm.plugins' },
     { import = 'sergevm.plugins.lsp' },
   },
-  install = { colorscheme = { 'tokyonight' } },
+  -- install = { colorscheme = { 'catppuccin' } },
   -- automatically check for plugin updates
   change_detection = { notify = false },
   checker = { enabled = true, notify = false },
+  colorscheme = 'catppuchin',
 }
