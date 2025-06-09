fdcaab7c83b27f011a2c836b42655ce444b2256f
Serge Van Meerbeeck <serge.vanmeerbeeck@gmail.com>
2025-06-09 10:53:40 +0200

    disabled precognition, added copilot.lua

---
+++ lua/sergevm/plugins/copilot.lua
@@ -0,0 +1,70 @@
+local logger = {
+  file = vim.fn.stdpath 'log' .. '/copilot-lua.log',
+  file_log_level = vim.log.levels.OFF,
+  print_log_level = vim.log.levels.WARN,
+  trace_lsp = 'off', -- "off" | "messages" | "verbose"
+  trace_lsp_progress = false,
+  log_lsp_messages = false,
+}
+return {
+  'zbirenbaum/copilot.lua',
+  cmd = 'Copilot',
+  event = 'InsertEnter',
+
+  config = function()
+    require('copilot').setup {
+
+      suggestion = {
+        enabled = true,
+        auto_trigger = true,
+        debounce = 75,
+        keymap = {
+          accept = '<C-e>',
+          next = '<C-n>',
+          prev = '<C-p>',
+          dismiss = '<C-c>',
+        },
+      },
+
+      filetypes = {
+        ['*'] = true,
+        ['markdown'] = false,
+        ['text'] = false,
+        ['TelescopePrompt'] = false,
+        ['toggleterm'] = false,
+        ['help'] = false,
+        ['qf'] = false,
+        ['Trouble'] = false,
+      },
+
+      logger = logger,
+
+      panel = {
+        enabled = true,
+        auto_refresh = true,
+        keymap = {
+          accept = '<C-e>',
+          jump_next = 'C-n',
+          jump_prev = 'C-p',
+          refresh = 'gr',
+          open = '<C-p>',
+        },
+      },
+
+      should_attach = function(_, bufname)
+        if string.match(bufname, 'env') then
+          return false
+        end
+        if not vim.bo.buflisted then
+          return false
+        end
+
+        if vim.bo.buftype ~= '' then
+          return false
+        end
+
+        return true
+      end,
+    }
+  end,
+}
