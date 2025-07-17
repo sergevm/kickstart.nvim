655369f7c7dbd2ec0630e0d247feaa212703df2a
Serge Van Meerbeeck <serge.vanmeerbeeck@gmail.com>
2025-07-17 17:04:55 +0200

    Fixing the double display of diagnostics
    Overriding the keymaps for navigating between diagnostics, where we only
    want to show the tiny inline diagnostics plugin diagnostic.

--- lua/sergevm/core/keymaps.lua
+++ lua/sergevm/core/keymaps.lua
@@ -39,6 +39,22 @@ keymap.set('n', '<leader>tf', 'u-[[ <cmd>tabnew %<C ]]R>', { desc = 'Open curren
 --   return vim.fn.pumvisible() and '<C-n>' or 'k'
 -- end, { silent = true, expr = true })
 
+-- Override diagnostic keymaps. Specifically in collaboration with tiny inline diagnostics. We don't want to show the built-in inline diagnostics, so we need float to be false.
+-- Doing this here, because it looks like a plugin sets somewhere the keymaps to jump to diagnostics, but it does not set float = false, which is needed for inline diagnostics.
+vim.api.nvim_create_autocmd('VimEnter', {
+  desc = 'Override diagnostic keymaps on startup',
+  group = vim.api.nvim_create_augroup('custom-diagnostic-maps', { clear = true }),
+  callback = function()
+    vim.keymap.set('n', ']d', function()
+      vim.diagnostic.jump { count = 1, float = false }
+    end, { desc = 'Go to next diagnostic' })
+
+    vim.keymap.set('n', '[d', function()
+      vim.diagnostic.jump { count = -1, float = false }
+    end, { desc = 'Go to previous diagnostic' })
+  end,
+})
+
 vim.api.nvim_create_autocmd('TextYankPost', {
   desc = 'Highlight when yanking (copying) text',
   group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
