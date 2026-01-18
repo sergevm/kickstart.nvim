f8f06c91c9b0d4e29b033b7d84d9512610676609
Serge Van Meerbeeck <serge.vanmeerbeeck@gmail.com>
2026-01-18 18:06:13 +0100

    Disable copilot for files > 1MB

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -45,7 +45,13 @@ return {
         enabled = false,
       },
 
-      should_attach = function(_, bufname)
+      should_attach = function(bufnr, bufname)
+        -- disable for large files
+        local ok, stats = pcall(vim.uv.fs_stat, vim.api.nvim_buf_get_name(bufnr))
+        if ok and stats and stats.size > 1024 * 1024 then -- 1MB
+          return false
+        end
+
         if string.match(bufname, 'env') then
           return false
         end
